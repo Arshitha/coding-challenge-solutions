70a98d39b1ca8f55b34a5680770a579b097efce6
Arshitha <arsh2794@gmail.com>
2017-12-13 20:25:30 -0500

    Update Day3_ConditionalStatements.py

--- Day3_ConditionalStatements.py
+++ Day3_ConditionalStatements.py
@@ -9,7 +9,7 @@ if N%2 == 0:
     if N >=2 and N <=5:
         print("Not Weird")
     elif N >=6 and N<=20:
-        print("Not Weird")
+        print("Weird")
     elif N > 20:
         print("Not Weird")
 else:
