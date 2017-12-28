88eb59c4087deac48485cfda3f0abc983679e62b
Arshitha <arsh2794@gmail.com>
2017-12-28 00:33:44 -0500

    Update Day12_Inheritance.py
    Problem Description:
    https://www.hackerrank.com/challenges/30-inheritance/problem

--- Day12_Inheritance.py
+++ Day12_Inheritance.py
@@ -8,34 +8,30 @@ class Person:
 		print("ID:", self.idNumber)
     
 class Student(Person):
-    def __init__(self, firstName, lastName, id, scores ):
-#   Class Constructor
-#   
-#   Parameters:
-#   firstName - A string denoting the Person's first name.
-#   lastName - A string denoting the Person's last name.
-#   id - An integer denoting the Person's ID number.
-#   scores - An array of integers denoting the Person's test scores.
-#
-# Write your constructor here
+    def __init__(self, firstName, lastName, id,scores ):
+        Person.__init__(self, firstName, lastName, id)
+        #for i in scores:
+        #    print(i)
     
-#   Function Name: calculate
-#   Return: A character denoting the grade.
-#
-# Write your function here
-    
-    
-
-
-
-
-
-
-
-
-
-
-
+    def calculate(self):
+        sumScore = 0
+        for i in scores:
+            #print(i)
+            sumScore = sumScore + i
+        avg = sumScore/len(scores)
+        #avg = int(avg)
+        if avg >= 90 and avg <= 100:
+            return 'O'
+        elif avg >= 80 and avg <90:
+            return 'E'
+        elif avg >=70 and avg <80:
+            return 'A'
+        elif avg >=55 and avg <70:
+            return 'P'
+        elif avg >= 40 and avg <55:
+            return 'D'
+        else:
+            return 'T'
 
 line = input().split()
 firstName = line[0]
