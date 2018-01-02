57d592063236fa4f3aa02371115e5a4f44faf655
Arshitha <arsh2794@gmail.com>
2018-01-01 23:56:00 -0500

    Update Day14_Scope.py
    partially completed code

--- Day14_Scope.py
+++ Day14_Scope.py
@@ -1,6 +1,17 @@
 class Difference:
     def __init__(self, a):
         self.__elements = a
+    
+    def computeDifference(self):
+        diff = []
+        print(len(a))
+        for i in range(0, len(a)-1):
+            for j in range(i+1,len(a)):
+                diff.append(abs(a[i]-a[j]))
+        print(sorted(diff))
+    
+    def maximumDifference(diff):
+        print(diff)
         
         
 # End of Difference class
