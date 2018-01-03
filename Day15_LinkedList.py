fea34417322ff699c1f9ff8b313a25021381f6ef
Arshitha <arsh2794@gmail.com>
2018-01-03 01:39:13 -0500

    Create Day15_LinkedList.py
    Problem Description:
    https://www.hackerrank.com/challenges/30-linked-list/problem

---
+++ Day15_LinkedList.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+    def insert(self,head,data):
+        # Initially when the linked list is empty
+        if head == None:
+            return Node(data)
+        # Traverses through existing nodes and adds it to the tail
+        else:
+            currNode = head
+            while currNode.next != None:                                
+                currNode = currNode.next           
+            currNode.next = Node(data)
+        
+        return head
+
+# Input Handling
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head);
