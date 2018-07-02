73e5d9ad1e3d7313c08e8b968bc5460ac9b66299
Arshitha Basavaraj <arsh2794@gmail.com>
2018-07-02 11:22:33 -0400

    Update Day26_NestedLogic.py
    Problem Description:
    https://www.hackerrank.com/challenges/30-nested-logic/problem
    
    all cases passed

--- Day26_NestedLogic.py
+++ Day26_NestedLogic.py
@@ -8,13 +8,22 @@ for line in sys.stdin:
 actual_date = dates[0].split(" ")
 expected_date = dates[1].split(" ")
 
-if actual_date[0] != expected_date[0] and actual_date[1] == expected_date[1] and actual_date[2]==expected_date[2]:
-    fine = 15 * (int(actual_date[0]) - int(expected_date[0]))
-elif actual_date[1] != expected_date[1] and actual_date[2]==expected_date[2]:
-    fine = 500 * (int(actual_date[1]) - int(expected_date[1]))
-elif actual_date[2]!=expected_date[2]:
-    fine = 10000
-elif actual_date[0] == expected_date[0] and actual_date[1] == expected_date[1] and actual_date[2]==expected_date[2]:
+actual_day = int(actual_date[0])
+actual_month = int(actual_date[1])
+actual_year = int(actual_date[2])
+
+expected_day = int(expected_date[0])
+expected_month = int(expected_date[1])
+expected_year = int(expected_date[2])
+
+fine = 0
+if (actual_day < expected_day or actual_day == expected_day) and actual_month == expected_month and actual_year == expected_year:
     fine = 0
+elif actual_day > expected_day and actual_month == expected_month and actual_year == expected_year:
+    fine = 15 * (actual_day - expected_day)
+elif actual_month > expected_month and actual_year == expected_year:
+    fine = 500 * (actual_month - expected_month)
+elif actual_year > expected_year:
+    fine = 10000
     
-print(fine)
+print (fine)
